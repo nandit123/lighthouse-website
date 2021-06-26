b56c4e3000c8434fdd4d7b276f9c44ac11bd6d16
nandit123 <nanditmehra123@gmail.com>
2021-06-26 10:55:50 +0530

    Added tab pages

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 window.addEventListener("load", Ready); 
 
+function openPage(pageName,elmnt) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+  document.getElementById(pageName).style.display = "block";
+  elmnt.style.backgroundColor = "#4d1170";
+}
+
 function Ready () {
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
+
   if (window.File && window.FileReader) {
     console.log('ready');
     document.getElementById("UploadButton").addEventListener('click', StartUpload);

443c2399c35a760b6794d0f9a6d2aa5db45e1a5e
nandit123 <nanditmehra123@gmail.com>
2021-08-06 01:21:41 +0530

    lint and formatting

--- index.js
+++ index.js
@@ -1,58 +1,16 @@
-window.addEventListener('load', Ready);
-
-function openPage(pageName, elmnt) {
-  let i; let tabcontent; let
-    tablinks;
-  tabcontent = document.getElementsByClassName('tabcontent');
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = 'none';
-  }
-  tablinks = document.getElementsByClassName('tablink');
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].style.backgroundColor = '';
-  }
-  document.getElementById(pageName).style.display = 'block';
-  elmnt.style.backgroundColor = '#4d1170';
-}
-
-function Ready() {
-  // Get the element with id="defaultOpen" and click on it
-  document.getElementById('defaultOpen').click();
-
-  if (window.File && window.FileReader) {
-    console.log('ready');
-    document.getElementById('UploadButton').addEventListener('click', StartUpload);
-    document.getElementById('FileBox').addEventListener('change', FileChosen);
-  } else {
-    document.getElementById('UploadBox').innerHTML = "Your Browser Doesn't Support The File API Please Update Your Browser";
-  }
-}
-
+// const socket = new io('http://13.233.207.237:3002'); // hosted
+const socket = new io('http://127.0.0.1:3002'); // local
+let FReader;
+let Name;
 let SelectedFile;
 let totalFiles;
 let Files;
 let filesProcessed = 0;
 let collectionSize = 0;
 let path;
-function FileChosen(event) {
-  document.getElementById('FileCid').innerHTML = '';
-  document.getElementById('FolderCid').innerHTML = '';
-  document.getElementById('loader').style.display = 'none';
-  path = Date.now().toString();
-  SelectedFile = event.target.files[0];
-  totalFiles = event.target.files.length;
-  Files = event.target.files;
-  document.getElementById('NameBox').value = SelectedFile.name;
-  filesProcessed = 0;
-  collectionSize = 0;
-}
 
-const socket = new io('http://13.233.207.237:3002'); // hosted
-// const socket = new io("http://127.0.0.1:3002"); // local
-let FReader;
-let Name;
 function StartUpload() {
-  if (document.getElementById('FileBox').value != '') {
+  if (document.getElementById('FileBox').value !== '') {
     try {
       FReader = new FileReader();
       SelectedFile = Files[filesProcessed];
@@ -69,24 +27,63 @@ function StartUpload() {
       console.log('error:', error);
     }
   } else {
-    alert('Please Select A File');
+    console.log('Please Select A File');
   }
+}
+
+function FileChosen(event) {
+  document.getElementById('FileCid').innerHTML = '';
+  document.getElementById('FolderCid').innerHTML = '';
+  document.getElementById('loader').style.display = 'none';
+  path = Date.now().toString();
+  SelectedFile = event.target.files[0];
+  totalFiles = event.target.files.length;
+  Files = event.target.files;
+  document.getElementById('NameBox').value = SelectedFile.name;
+  filesProcessed = 0;
+  collectionSize = 0;
+}
+
+function Ready() {
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById('defaultOpen').click();
+
+  if (window.File && window.FileReader) {
+    document.getElementById('UploadButton').addEventListener('click', StartUpload);
+    document.getElementById('FileBox').addEventListener('change', FileChosen);
+  } else {
+    document.getElementById('UploadBox').innerHTML = "Your Browser Doesn't Support The File API Please Update Your Browser";
+  }
+}
+
+function bytesToSize(bytes) {
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+  if (bytes === 0) return '0 Byte';
+  const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+  return `${Math.round(bytes / Math.pow(1024, i), 2)} ${sizes[i]}`;
+}
+
+window.addEventListener('load', Ready);
+
+function openPage(pageName, elmnt) {
+  const tabcontent = document.getElementsByClassName('tabcontent');
+  let i;
+  for (i = 0; i < tabcontent.length; i += 1) {
+    tabcontent[i].style.display = 'none';
+  }
+  const tablinks = document.getElementsByClassName('tablink');
+  for (i = 0; i < tablinks.length; i += 1) {
+    tablinks[i].style.backgroundColor = '';
+  }
+  document.getElementById(pageName).style.display = 'block';
+  const currentElement = elmnt;
+  currentElement.style.backgroundColor = '#4d1170';
 }
 
 function getCidSize() {
   const cid = document.getElementById('cidInput').value;
   socket.emit('GetCidSize', cid);
 }
-
-socket.on('CidSize', (data) => {
-  if (data.size == 'Error') {
-    console.log('error:', data.size);
-  } else {
-    const { size } = data;
-    const sizeFormatted = bytesToSize(size);
-    filCostInEth(size, sizeFormatted); // get's current price of FIL in ETH
-  }
-});
 
 const filCostInEth = async (size, sizeFormatted) => { // function to get FIL cost in ETH
   const costInFil = 0.0000011972551454466497; // FIL / GB /Year
@@ -98,54 +95,18 @@ const filCostInEth = async (size, sizeFormatted) => { // function to get FIL cos
   document.getElementById('storageCostDiv').style.display = 'block';
   document.getElementById('submitButton').style.display = 'block';
   document.getElementById('converstonRate').innerHTML = `${conversionRate.toFixed(5)} ETH`;
-  console.log('size of cid:', size, ` (${sizeFormatted})`);
   document.getElementById('cidSize').innerHTML = sizeFormatted;
   document.getElementById('cidCost').innerHTML = ((size * costInFil * conversionRate) / (1024 * 1024 * 1024)).toFixed(18);
 };
 
-socket.on('MoreData', (data) => {
-  UpdateBar(data.Percent);
-  const Place = data.Place * 524288; // The Next Blocks Starting Position
-  let NewFile; // The Variable that will hold the new Block of Data
-  if (SelectedFile.slice) NewFile = SelectedFile.slice(Place, Place + Math.min(524288, (SelectedFile.size - Place)));
-  else NewFile = SelectedFile.slice(Place, Place + Math.min(524288, (SelectedFile.size - Place)));
-  FReader.readAsBinaryString(NewFile);
-});
-
-socket.on('FileDownloaded', (data) => {
-  if (filesProcessed < totalFiles) {
-    SelectedFile = Files[filesProcessed];
-    filesProcessed += 1;
-    UpdateBar(0);
-    StartUpload();
-    if (filesProcessed == totalFiles) {
-      // emit here for get info
-      socket.emit('GetCid', path);
-      document.getElementById('UploadArea').innerHTML = '';
-	  document.getElementById('loader').style.display = 'block';
-    }
+socket.on('CidSize', (data) => {
+  if (data.size === 'Error') {
+    console.log('error:', data.size);
+  } else {
+    const { size } = data;
+    const sizeFormatted = bytesToSize(size);
+    filCostInEth(size, sizeFormatted); // get's current price of FIL in ETH
   }
-});
-
-function bytesToSize(bytes) {
-  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-  if (bytes == 0) return '0 Byte';
-  const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
-  return `${Math.round(bytes / Math.pow(1024, i), 2)} ${sizes[i]}`;
-}
-
-socket.on('FileInfo', (data) => {
-  collectionSize += data.size;
-
-  document.getElementById('FileCid').innerHTML = `<b>Size:</b> ${bytesToSize(collectionSize)}<br>`;
-  if (filesProcessed == totalFiles) {
-    document.getElementById('UploadArea').innerHTML = '';
-  }
-});
-
-socket.on('FolderCid', (data) => {
-  document.getElementById('loader').style.display = 'none';
-  document.getElementById('FolderCid').innerHTML = `<b> Collection CID: ${data.cid}<br>`;
 });
 
 function UpdateBar(percent) {
@@ -154,6 +115,45 @@ function UpdateBar(percent) {
   const MBDone = Math.round(((percent / 100.0) * SelectedFile.size) / 1048576);
   document.getElementById('MB').innerHTML = MBDone;
 }
+
+socket.on('MoreData', (data) => {
+  UpdateBar(data.Percent);
+  const Place = data.Place * 524288; // The Next Blocks Starting Position
+  let NewFile; // The Variable that will hold the new Block of Data
+  if (SelectedFile.slice) {
+    NewFile = SelectedFile.slice(Place, Place + Math.min(524288, (SelectedFile.size - Place)));
+  } else NewFile = SelectedFile.slice(Place, Place + Math.min(524288, (SelectedFile.size - Place)));
+  FReader.readAsBinaryString(NewFile);
+});
+
+socket.on('FileDownloaded', () => {
+  if (filesProcessed < totalFiles) {
+    SelectedFile = Files[filesProcessed];
+    filesProcessed += 1;
+    UpdateBar(0);
+    StartUpload();
+    if (filesProcessed === totalFiles) {
+      // emit here for get info
+      socket.emit('GetCid', path);
+      document.getElementById('UploadArea').innerHTML = '';
+      document.getElementById('loader').style.display = 'block';
+    }
+  }
+});
+
+socket.on('FileInfo', (data) => {
+  collectionSize += data.size;
+
+  document.getElementById('FileCid').innerHTML = `<b>Size:</b> ${bytesToSize(collectionSize)}<br>`;
+  if (filesProcessed === totalFiles) {
+    document.getElementById('UploadArea').innerHTML = '';
+  }
+});
+
+socket.on('FolderCid', (data) => {
+  document.getElementById('loader').style.display = 'none';
+  document.getElementById('FolderCid').innerHTML = `<b> Collection CID: ${data.cid}<br>`;
+});
 
 const contractAbi = ([
   {
@@ -210,16 +210,11 @@ const contractAbi = ([
 ]);
 
 let ethaddress;
-let web3;
 
 async function connectWallet() {
-  if (window.ethereum) {
-    web3 = new ethers.providers.Web3Provider(window.ethereum);
-  }
+  const conn = await window.ethereum.enable();
 
-  conn = await window.ethereum.enable();
-
-  ethconnected = conn.length > 0;
+  const ethconnected = conn.length > 0;
   if (ethconnected) {
     ethaddress = conn[0]; // get wallet address
     console.log('wallet address:', ethaddress);
@@ -238,7 +233,6 @@ async function load() {
 function callContract() {
   document.getElementById('sentCid').innerHTML = '';
   document.getElementById('tHash').innerHTML = '';
-  // window.web3 = new Web3("https://rinkeby.infura.io/v3/3d635004c08743daae3a5cb579559dbd");
   const provider = new ethers.providers.Web3Provider(window.ethereum);
   const signer = provider.getSigner();
   const cid = document.getElementById('cidInput').value;
@@ -256,8 +250,8 @@ function getStorageInfo() {
   document.getElementById('storageBrief').style.display = 'none';
   const cid = document.getElementById('cidInput2').value;
 
-  const socket = new io('http://13.233.207.237:3002'); // hosted
-  // const socket = new io("http://127.0.0.1:3002"); // local
+  // const socket = new io('http://13.233.207.237:3002'); // hosted
+  const socket = new io('http://127.0.0.1:3002'); // local
   // handle the event sent with socket.send()
   socket.on('message', (data) => {
     console.log(data);
@@ -267,10 +261,10 @@ function getStorageInfo() {
     socket.emit('cid', cid);
   });
 
-  socket.on('storageInfo', (storageInfo) => {
+  socket.on('storageInfo', (data) => {
     document.getElementById('storageBrief').style.display = 'none';
-    if (!storageInfo.storageInfo) {
-      storageInfo = jQuery.parseJSON(storageInfo);
+    if (!data.storageInfo) {
+      const storageInfo = jQuery.parseJSON(data);
       const textedJson = JSON.stringify(storageInfo, undefined, 4);
       document.getElementById('storageInfo').innerHTML = textedJson;
       document.getElementById('storageBrief').style.display = 'block';
@@ -283,7 +277,7 @@ function getStorageInfo() {
         document.getElementById('storageBrief').innerHTML = '<b>Deal Status: </b> Not made. Try Again later or with different config';
       }
     } else {
-      document.getElementById('storageInfo').innerHTML = storageInfo.storageInfo.toString();
+      document.getElementById('storageInfo').innerHTML = data.storageInfo.toString();
     }
     socket.disconnect();
   });
